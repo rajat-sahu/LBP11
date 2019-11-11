fd453cfd53788d487ebdc375b4743cec2227d6d9
RAJATSAHU <RAJATSAHU@9.81.199.85>
2019-11-11 16:16:39 +0530

    This is my first changes to Test Case 1

---
+++ final-framework-testng/assignment/simple/RETC_001.java
@@ -0,0 +1,19 @@
+package simple;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class RETC_001 {
+	@Test
+	public void login() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://realestatem1.upskills.in/");
+		driver.quit();
+	}
+
+}
